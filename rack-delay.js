4a3dd59ea125bde35c89fdca9a6d6f7c735cf04b
Rui Lopes <ruidlopes@gmail.com>
2013-02-17 12:19:20 -0800

    Complete delay implementation

--- rack-delay.js
+++ rack-delay.js
@@ -11,15 +11,73 @@ rack.delay.Delay = function(rackInstance) {
   this.input.gain.value = 1.0;
   this.output.gain.value = 1.0;
   this.input.connect(this.output);
+  this.knobs.push(new rack.Knob(this, 'taps', -100, 35, '#000', '#333'));
+  this.knobs.push(new rack.Knob(this, 'time', -200, 35, '#000', '#333'));
+
   this.delays = [];
+  this.activeDelays = 0;
+  this.tapTime = 0;
+  this.createDelays();
 };
 lib.inherits(rack.delay.Delay, rack.Unit);
 
 rack.delay.Delay.MAX_DELAYS = 10;
 
+rack.delay.Delay.prototype.createDelays = function() {
+  for (var i = 0; i < rack.delay.Delay.MAX_DELAYS; ++i) {
+    this.delays.push(new rack.delay.DelayLine(this));
+  }
+  this.updateDelays();
+};
+
+rack.delay.Delay.prototype.updateDelays = function() {
+  for (var i = 0; i < rack.delay.Delay.MAX_DELAYS; ++i) {
+    if (i < this.activeDelays) {
+      this.delays[i].connect(this.tapTime * i, 1.0 - (i/this.activeDelays));
+    } else {
+      this.delays[i].disconnect();
+    }
+  }
+};
+
+rack.delay.Delay.prototype.onKnobValueChanged = function(knob, newValue) {
+  switch (knob.name) {
+    case 'taps':
+      this.activeDelays = Math.floor(newValue * 10);
+      this.updateDelays();
+      break;
+    case 'time':
+      this.tapTime = newValue * 5;
+      this.updateDelays();
+      break;
+  }
+  this.render();
+};
+
 rack.delay.Delay.prototype.renderComponents = function() {
   this.renderPlate('#00c', '03f', 'Delay', '33,33,33');
   rack.Unit.prototype.renderComponents.call(this);
+};
+
+
+namespace('rack.delay.DelayLine');
+rack.delay.DelayLine = function(delay) {
+  this.delay = delay;
+  this.delayNode = this.delay.rack.context.createDelay(5.0 /** max seconds for buffering */);
+  this.gainNode = this.delay.rack.context.createGain();
+  this.gainNode.gain.value = 1.0;
+  this.delay.input.connect(this.delayNode);
+  this.delayNode.connect(this.gainNode);
+};
+
+rack.delay.DelayLine.prototype.connect = function(opt_time, opt_gain) {
+  this.delayNode.delayTime.value = opt_time || 0.0;
+  this.gainNode.gain.value = opt_gain || 1.0;
+  this.gainNode.connect(this.delay.output);
+};
+
+rack.delay.DelayLine.prototype.disconnect = function() {
+  this.gainNode.disconnect();
 };
 
 // register this rack unit.
